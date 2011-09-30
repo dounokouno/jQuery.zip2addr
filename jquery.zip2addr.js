ff8bfe5ef422c0360e068556d104032824911f9c
tagawa <dounokouno@gmail.com>
2011-09-30 15:23:29 +0900

    クリックによる住所入力に対応
    ・インデントの混在を修正
    ・ダウンロードした sample.html をブラウザで開いた場合に動作しない不具合を修正
    ・クリックによる住所入力に対応
    ・クリック対応により、引数を Object に統一
    ・sample.html にボタンクリックの場合を追加

--- jquery.zip2addr.js
+++ jquery.zip2addr.js
@@ -10,47 +10,64 @@
  *	jQuery 1.4 or above
  */
 
-$.fn.zip2addr = function(target){
-    var c = {
-		api: location.protocol + '//www.google.com/transliterate?langpair=ja-Hira|ja&jsonp=?',
+/*
+ * jQuery.zip2addr with click
+ *
+ * Copyright 2011, Takao Tagawa dounokouno@gmail.com
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * https://github.com/dounokouno/jQuery.zip2addr/branches/with_click
+ *
+ */
+
+$.fn.zip2addr = function(options){ 
+	var c = $.extend({
+		api: 'http://www.google.com/transliterate?langpair=ja-Hira|ja&jsonp=?',
 		prefectureToken: '(東京都|道|府|県)',
-		zipDelimiter: '-'
-    }
-
+		zipDelimiter: '-',
+		zip: null,
+		zip2: null,
+		pref: null,
+		addr: null,
+		event: 'change',
+		btn: null
+	}, options);
+	
 	var cache = $.fn.zip2addr.cache;
-
-    var getAddr = function(zip,callback){
-        $.getJSON(c.api,{'text':zip},
-			function(json){
-				if(RegExp(c.prefectureToken).test(json[0][1][0])){
-					callback(json[0][1][0].replace(RegExp('(.*?'+c.prefectureToken+')(.+)'),function(a,b,c,d){return [b,d]}))
-				}
+	
+	var getAddr = function(zip, callback){
+		if (location.protocol == 'https:') {
+			c.api = c.api.match(/http:/, 'https:');
+		}
+		$.getJSON(c.api, {'text':zip}, function(json){
+			if (RegExp(c.prefectureToken).test(json[0][1][0])) {
+				callback(json[0][1][0].replace(RegExp('(.*?'+c.prefectureToken+')(.+)'),function(a,b,c,d){return [b,d]}))
 			}
-		)
+		});
 	}
 	
 	var fillAddr = (function(){
-		if(typeof target == 'object' && target.pref){
+		if (c.pref) {
 			return function(addr){
 				var addrs = addr.split(',');
-				if(addrs){
-					if(!RegExp(addrs[1]).test($(target.addr).val())){
-						$(target.pref).val(addrs[0]);
-						$(target.addr).val(addrs[1]);
+				if (addrs) {
+					if (!RegExp(addrs[1]).test($(c.addr).val())) {
+						$(c.pref).val(addrs[0]);
+						$(c.addr).val(addrs[1]);
 					}
-				}else if(!RegExp(addrs[1]).test($(target.addr).val())){
-					$(target.pref).add(target.addr).val('');
+				} else if (!RegExp(addrs[1]).test($(c.addr).val())) {
+					$(c.pref).add(c.addr).val('');
 				}
 			}
-		}else{
+		} else {
 			return function(addr){
 				var addrStr = addr.replace(',','');
-				var addrField = target.addr || target;
-				if(addrStr){
-					if(!RegExp(addrStr).test($(addrField).val())){
+				var addrField = c.addr;
+				if (addrStr) {
+					if (!RegExp(addrStr).test($(addrField).val())) {
 						$(addrField).val(addrStr);
 					}
-				}else if(!RegExp(addrStr).test($(addrField).val())){
+				} else if (!RegExp(addrStr).test($(addrField).val())) {
 					$(addrField).val('');
 				}
 			}
@@ -58,7 +75,7 @@ $.fn.zip2addr = function(target){
 	})()
 
 	//From http://liosk.blog103.fc2.com/blog-entry-72.html
-	var fascii2ascii = (function() {
+	var fascii2ascii = (function(){
 		var pattern = /[\uFF01-\uFF5E]/g, replace = function(m) {
 			return String.fromCharCode(m.charCodeAt() - 0xFEE0);
 		};
@@ -67,44 +84,62 @@ $.fn.zip2addr = function(target){
 
 	var check = function(_val){
 		var val = fascii2ascii(_val).replace(/\D/g,'');
-		if(val.length == 7){
-			if(cache[val] == undefined){
+		if (val.length == 7) {
+			if (cache[val] == undefined) {
 				getAddr(val.replace(/(\d\d\d)(\d\d\d\d)/,'$1-$2'),function(json){
 					cache[val] = json;
 					fillAddr(json);
 				})
-			}else{
+			} else {
 				fillAddr(cache[val]);
 			}
 		}
 	}
-
-    this.each(function(){
-        var elem = $(this);
-		if(typeof target == 'object' && target.zip2){
-			elem.add($(target.zip2))
-				.bind('keyup.zip2addr change.zip2addr',function(){
-					check(elem.val()+''+$(target.zip2).val())
-				})
-				.bind('blur.zip2addr',function(){
-					$(this).val(function(){
-						return fascii2ascii($(this).val())
+	
+	this.each(function(){
+		var elem = $(this);
+		if (c.event == 'click') {
+			if (c.zip2) {
+				$(c.btn).click(function(){
+					check(elem.val()+''+$(c.zip2).val());
+					elem.val(function(){
+						return fascii2ascii(elem.val()).replace(/\D/g,'').replace(/(\d\d\d)(\d\d\d\d)/,'$1'+c.zipDelimiter+'$2');
 					})
-				})
-		}else{
-			elem
-				.bind('keyup.zip2addr change.zip2addr',function(){
-					check(elem.val())
-				})
-				.bind('blur.zip2addr',function(){
-					$(this).val(function(){
-						return fascii2ascii($(this).val()).replace(/\D/g,'').replace(/(\d\d\d)(\d\d\d\d)/,'$1'+c.zipDelimiter+'$2')
+				});
+			} else {
+				$(c.btn).click(function(){
+					check(elem.val());
+					elem.val(function(){
+						return fascii2ascii(elem.val()).replace(/\D/g,'').replace(/(\d\d\d)(\d\d\d\d)/,'$1'+c.zipDelimiter+'$2');
 					})
-				})
+				});
+			}
+		} else {
+			if (c.zip2) {
+				elem.add($(c.zip2))
+					.bind('keyup.zip2addr change.zip2addr',function(){
+						check(elem.val()+''+$(c.zip2).val());
+					})
+					.bind('blur.zip2addr',function(){
+						elem.val(function(){
+							return fascii2ascii(elem.val());
+						})
+					})
+			} else {
+				elem
+					.bind('keyup.zip2addr change.zip2addr',function(){
+						check(elem.val());
+					})
+					.bind('blur.zip2addr',function(){
+						elem.val(function(){
+							return fascii2ascii(elem.val()).replace(/\D/g,'').replace(/(\d\d\d)(\d\d\d\d)/,'$1'+c.zipDelimiter+'$2');
+						})
+					})
+			}
 		}
-    });
-
-    return this;
+	});
+	
+	return this;
 };
 
 $.fn.zip2addr.cache = {};
